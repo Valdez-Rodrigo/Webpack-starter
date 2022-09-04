1f160ac641386be66d32e044cd8a31ce3d270b42
Rodrigo Valdez <joserodrigovalde@gmail.com>
2022-09-03 22:17:34 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-
-import { saludar } from './js/components';
 import './style.css';
 
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/components';
 
-const nombre = 'Rodrigo'
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
+
+console.log('todos', todoList);
+
+// const tarea = new Todo('Aprender');
+// todoList.nuevoTodo( tarea );
+
+// console.log(todoList);
+
+// crearTodoHtml( tarea );
+
+
+//local storage aguanta un reinicio de la computadora, guardar informacion para la aplicacion (no contraseña)
+//Session storage se va a borrar todo cuando se cierra el navegador web
+
+// localStorage.setItem('mi-key', 'ABC1234');
+// sessionStorage.setItem('mi-key', 'ABC1234');
+
+// setTimeout(() =>{
+    
+//     localStorage.removeItem('mi-key')
+
+// }, 1500);
+
 
